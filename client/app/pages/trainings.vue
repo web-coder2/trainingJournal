4dd55641dad509c2b724b84151885737a5daf9a1
web-coder <yasher18@bk.ru>
2026-08-05 00:58:08 +0300

    сделал рендер списка тренировок из даных по api

--- client/app/pages/trainings.vue
+++ client/app/pages/trainings.vue
@@ -3,7 +3,7 @@
         <h3>{{ title }}</h3>
         <div class="mt-5">
             <h5>Список тренировок</h5>
-            <div class="list-group mt-5">
+            <div class="list-group mt-5" v-if="trainingsList">
                 <div class="list-group-item" v-for="(item, index) in trainingsList">
                     <strong>{{ item.user }}</strong>
                     <p>Список сетов за тренировку</p>
@@ -31,17 +31,22 @@
         data() {
             return {
                 title: "вкладка тренировок",
-                trainingsList: [
-                    { 
-                        user: 'w3b-c0d3r', 
-                        date: '2026-01-01', 
-                        sets: [
-                            { workoutName: 'Прыжки на коробку', countRetry: 15, countRounds: 5 }
-                        ],
-                        trainingSuccess: true
-                    }
-                ]
+                trainingsList: null,
             }
+        },
+        methods: {
+            async fetchAllTrainings() {
+                try {
+                    const response = await fetch('http://localhost:3000/trainings/getAll')
+                    const result = await response.json()
+                    this.trainingsList = result
+                } catch (e) {
+                    console.log(e.message)
+                }
+            }
+        },
+        async beforeMount() {
+            await this.fetchAllTrainings()
         }
     }
 
--- server/src/trainings/trainings.service.ts
+++ server/src/trainings/trainings.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from "@nestjs/common";
 import type { createTrainingDto } from "src/types/trainings.types";
+import { workoutCategories } from "src/types/workout.types";
 
 @Injectable()
 export class TrainigsService {
     private readonly trainings: createTrainingDto[] = [
         {
-            user: 'w3b-c0d3r', 
+            user: 'superuser', 
             date: '2026-01-01', 
-            sets: [], 
+            sets: [
+                {
+                    workoutName: 'Прыжки на коробку',
+                    countRetry: 10,
+                    countRounds: 5
+                }
+            ],
             trainingSuccess: true
         }
     ]
